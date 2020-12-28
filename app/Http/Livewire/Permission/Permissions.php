8aa4b1717d3a23c72eeca648796be9a6de7fd4f4
devscript-abdo <elmarzouguiabdelghafour@gmail.com>
2020-12-28 18:40:52 +0100

    add permission Livewire component

---
+++ app/Http/Livewire/Permission/Permissions.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Permission;
+
+use Livewire\Component;
+
+class Permissions extends Component
+{
+    public function render()
+    {
+        return view('livewire.permission.permissions');
+    }
+}
